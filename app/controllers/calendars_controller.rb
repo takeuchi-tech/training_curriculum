488d605f270eb3ace16e7fbcd5666a2e4a910050
takeuchi-tech <mt.tech00885@gmail.com>
2023-08-16 16:18:00 +0900

    Issue4～6　再修正
    シンボル型への修正

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -22,7 +22,6 @@ class CalendarsController < ApplicationController
     wdays = ['(日)','(月)','(火)','(水)','(木)','(金)','(土)']
     @wday = Date.today.wday
 
-
     # Dateオブジェクトは、日付を保持しています。下記のように`.today.day`とすると、今日の日付を取得できます。
     @todays_date = Date.today
     # 例)　今日が2月1日の場合・・・ Date.today.day => 1日
@@ -43,7 +42,7 @@ class CalendarsController < ApplicationController
         wday_num = @wday + x
       end
       
-      days = { :month => (@todays_date + x).month, :date => (@todays_date+x).day, :plans => today_plans, :wday => wdays[wday_num]}
+      days = { month: (@todays_date + x).month, date: (@todays_date+x).day, plans: today_plans, wday: wdays[wday_num]}
       @week_days.push(days)
     end
 
