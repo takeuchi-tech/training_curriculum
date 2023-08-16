7629f04ed8f1d7833240ebb9b1c3099bbbe68e93
takeuchi-tech <mt.tech00885@gmail.com>
2023-08-16 13:59:29 +0900

    Issue6 修正
     calendars_controller.rb の24行目がどこにもかかっていなかったため削除

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -21,7 +21,7 @@ class CalendarsController < ApplicationController
   def getWeek
     wdays = ['(日)','(月)','(火)','(水)','(木)','(金)','(土)']
     @wday = Date.today.wday
-    wday_str = wdays[@wday]
+
 
     # Dateオブジェクトは、日付を保持しています。下記のように`.today.day`とすると、今日の日付を取得できます。
     @todays_date = Date.today
